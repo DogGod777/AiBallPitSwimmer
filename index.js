a329967bf7e7a6575c91e5c463bed81560584095
CodingPheonix <agraw17107@gapps.uwcsea.edu.sg>
2020-12-18 08:24:25 +0800

    Fixed collision, opacity issues, and lines

--- index.js
+++ index.js
@@ -7,14 +7,23 @@ window.addEventListener('load', function() {
     const innerChunkAmt = 3;
     const outerChunkAmt = 2;
     const chunkSize = width/innerChunkAmt;
-    var offsetCounter = 0; 
+    var offsetCounter = 0;
 
     //Ball variables
     const ballWidth = 25;
     const ballRows = 5;
-    const ballCols = (innerChunkAmt + 2*outerChunkAmt)*chunkSize/ballWidth * 1/2-2;
-    const balls = false;
+    const ballCols = ((innerChunkAmt + 2*outerChunkAmt)*chunkSize)/ballWidth * 1/2 - 5;
+
+    //Environment variables
+    const balls = true;
     const diving = false;
+    var best;
+
+    //Camera Variables:
+    var opacity = 0.1; //changed opacity if not fittest
+    var xBuffer = 50; //If the difference is greater than this much extra, only then switch. So that the camera doesn't flutter that muchif multiple are close to the highest
+    var following = true; //Toggle follow fittest to fit everyone render camera
+    var renderBuffer = 100; // if fits everyone then this much is border from the bottom left and top right bounding box.
 
     //Aliases
     var Engine = Matter.Engine,
@@ -27,6 +36,7 @@ window.addEventListener('load', function() {
         Constraints = Matter.constraints,
         Events = Matter.Events,
         Bounds = Matter.Bounds;
+        Detector = Matter.Detector;
 
     //NEAT Aliases
 
@@ -49,7 +59,7 @@ window.addEventListener('load', function() {
     var USE_TRAINED_POP = false;
 
     var neat = new Neat(
-        4, 7,
+        4, 6,
         null,
         {
         mutation: [
@@ -82,6 +92,10 @@ window.addEventListener('load', function() {
 
     // create an engine
     var engine = Engine.create();
+
+    //If diving
+    var ballStack;
+    var divingPlatform = Bodies.rectangle(width/2, height-height/4, width/4, height/2, {isStatic: true});
     
     /** Start the evaluation of the current generation */
     //FIX: Make it so that the simulation is reset.
@@ -90,11 +104,30 @@ window.addEventListener('load', function() {
         players = [];
         highestScore = 0;
     
+        //Spawn balls
+        //BUG: Remove gaps behind the board
+        //Solution: Spawn balls half in front, half behind - (still doesn't work)
+        //If diving --> spawn diving board and position balls differently
+        if (diving){
+            ballStack = Composites.stack(width*5/8+50, -250, ballCols, ballRows, 0, 0, function(x, y) {
+                return Bodies.circle(x, y, ballWidth, {collisionFilter:{layer:0}});
+            });
+            Composite.add(engine.world, divingPlatform);
+        } else {
+            ballStack = Composites.stack(width/2 - ballCols*ballWidth + ballWidth/2, -250, ballCols, ballRows, 0, 0, function(x, y) {
+                return Bodies.circle(x, y, ballWidth, {collisionFilter:{layer:0}});
+            });
+        }
+
+        //if balls
+        if (balls){
+            Composite.add(engine.world, ballStack);
+        }
+
         for(var genome in neat.population){
             genome = neat.population[genome];
             agent = new Agent(genome)
             Composite.add(engine.world, agent.physics);
-
         }
     }
 
@@ -102,18 +135,18 @@ window.addEventListener('load', function() {
     startEvaluation();
 
     function endEvaluation(){
-        console.log('Generation:', neat.generation, '- average score:', Math.round(neat.getAverage()));
-        console.log('Fittest score:', Math.round(neat.getFittest().score));
-      
-        // Networks shouldn't get too big
-        for(var genome in neat.population){
-          genome = neat.population[genome];
+        // Evaluate final score and networks should not get to big
+        for(var i in neat.population){
+          genome = neat.population[i];
+          players[i].score();
           genome.score -= genome.nodes.length *  width / 10;
         }
       
+        console.log('Generation:', neat.generation, '- average score:', Math.round(neat.getAverage()));
+        console.log('Fittest score:', Math.round(neat.getFittest().score));
         // Sort the population by score
         neat.sort();
-      
+    
         // Init new pop
         var newPopulation = [];
       
@@ -134,6 +167,7 @@ window.addEventListener('load', function() {
         neat.generation++;
 
         players.forEach(player => Composite.remove(engine.world, player.physics))
+        Composite.remove(engine.world, ballStack);
         startEvaluation();
     }
 
@@ -150,43 +184,40 @@ window.addEventListener('load', function() {
                 hasBounds: true
             }
     });
-    
-    //BUG: Remove gaps behind the board
-    //Solution: Spawn balls half in front, half behind - (still doesn't work)
-    var divingPlatform = Bodies.rectangle(width/2, height-height/4, width/4, height/2, {isStatic: true});
-    //If diving --> spawn diving board and position balls differently
-    if (diving){
-        var ballStack = Composites.stack(width*5/8+50, -250, ballCols, ballRows, 0, 0, function(x, y) {
-            return Bodies.circle(x, y, ballWidth);
-        });
-        Composite.add(engine.world, divingPlatform);
-    } else {
-        var ballStack = Composites.stack(width/2 - ballCols*ballWidth + ballWidth/2, -250, ballCols, ballRows, 0, 0, function(x, y) {
-            return Bodies.circle(x, y, ballWidth);
-        });
-    }
 
     // create boundaries
-    var ground = Bodies.rectangle(width/2, height, width+4*chunkSize, 3*ballWidth, { isStatic: true });
-    var LWall = Bodies.rectangle(-2*chunkSize, height/2,  3*ballWidth, height, {isStatic: true});
-    var RWall = Bodies.rectangle(width+2*chunkSize, height/2,  3*ballWidth, height, {isStatic: true});
+    var ground = Bodies.rectangle(width/2, height, width+4*chunkSize, 3*ballWidth, {isStatic: true, collisionFilter:{layer:0}}); //Fix collsiion filtering system
+    var LWall = Bodies.rectangle(-2*chunkSize, height/2,  3*ballWidth, height, {isStatic: true, collisionFilter:{layer:0}});
+    var RWall = Bodies.rectangle(width+2*chunkSize, height/2,  3*ballWidth, height, {isStatic: true, collisionFilter:{layer:0}});
+
+
+    // UI Lines
+    var fittestLine = Bodies.rectangle(0, height/2, width/72, height, {isStatic: true, collisionFilter:{layer:-1}}) //Sort out collision filters - make it 100% non-collidable
+    var averageLine = Bodies.rectangle(0, height/2, width/72, height, {isStatic: true, collisionFilter:{layer:-1}})
 
     //Update Loop(s) --
 
-    //Update Camera (follow player)
-    Events.on(engine, 'beforeUpdate', function(event) { //Smooth camera? Dampening?
-        var agentBounds = Composite.bounds(players[0].physics) //edit these bounds so that player is centered at 1/3 height from bottom, not centered
+    //Update Camera (follow fittest player) //BUGGY
+    Events.on(engine, 'beforeUpdate', function(event) { //Stretch: make camera fit all agents 
+                                                        //EXTRA: make option between scale to fittest and all agents - toggle check mark box
+        best = players.reduce((prev, current) => (Composite.bounds(prev.physics).max.x > Composite.bounds(current.physics).max.x) ? prev : current);
+        players.forEach(agent => agent.physics.bodies.forEach(limb => limb.render.opacity = opacity))
+        best.physics.bodies.forEach(limb => limb.render.opacity = 1)
+        agentBounds = Composite.bounds(best.physics)
+        Body.setPosition(averageLine, {x: (players.reduce(function (a, b) { 
+            return a + Composite.bounds(b.physics).max.x
+        }, 0))/players.length+width/144, y:height/2})
+        Body.setPosition(fittestLine, {x: agentBounds.max.x+width/144, y:height/2})  //Add text arrow at top of line to the right
         agentBounds.min.y -= height/3;
         agentBounds.max.y -= height/3;
         Render.lookAt(render, agentBounds, {x: chunkSize*innerChunkAmt*1/2, y: height/3});
-
     });
  
     
     //Chunk render logic
     //IDEAS: case - everyone in the same simulation, avg max x rather than first max x
     Events.on(engine, 'afterUpdate', function(event) {
-        bounds = Composite.bounds(players[0].physics);
+        bounds = Composite.bounds(best.physics);
         if (bounds.max.x >=  width/2 + chunkSize/2 + offsetCounter){  // if agent moved to the right too far, move leftmost chunk to the far right
             for (i=0; i<ballStack.bodies.length; i++){
                 if(ballStack.bodies[i].bounds.max.x <= -chunkSize * (outerChunkAmt-1) + offsetCounter){
@@ -204,6 +235,7 @@ window.addEventListener('load', function() {
             [ground, LWall, RWall].forEach(body => Body.translate(body, {x: -chunkSize, y:0}))
             offsetCounter -= chunkSize;
         }
+
         for (i=0; i<ballStack.bodies.length; i++){  //If balls below the map
             if(ballStack.bodies[i].bounds.max.y >= height+3*ballWidth){
                 Body.setPosition(ballStack.bodies[i], {x: ballStack.bodies[i].x, y: height-ballWidth})
@@ -216,18 +248,14 @@ window.addEventListener('load', function() {
         }
 
         //Update agent position based based on brain outputs
-        players.forEach(player => {player.update(); 
-                                  player.score()});
+        players.forEach(player => player.update());
         
         iteration++;
     });
 
     // add all of the bodies to the world
-    if (balls){
-        Composite.add(engine.world, [ballStack, ground, LWall, RWall]);
-    } else {
-        Composite.add(engine.world, [ground, LWall, RWall])
-    }
+    Composite.add(engine.world, [fittestLine, averageLine, ground, LWall, RWall])
+  
     // run the engine
     Engine.run(engine);
     
@@ -249,16 +277,16 @@ window.addEventListener('load', function() {
        document.body.addEventListener("keydown", function(e){ 
         switch(e.which){
           case 65:
-            players[0].physics.bodies.forEach(body => Body.setVelocity(body, {x: -50, y: 0}));
+            best.physics.bodies.forEach(body => Body.setVelocity(body, {x: -50, y: 0}));
             break;
           case 68:
-            players[0].physics.bodies.forEach(body => Body.setVelocity(body, {x: 50, y: 0}));
+            best.physics.bodies.forEach(body => Body.setVelocity(body, {x: 50, y: 0}));
             break;
           case 87:
-            players[0].physics.bodies.forEach(body => Body.setVelocity(body, {x: 0, y: -50}));
+            best.physics.bodies.forEach(body => Body.setVelocity(body, {x: 0, y: -50}));
             break;
         case 83:
-            players[0].physics.bodies.forEach(body => Body.setVelocity(body, {x: 0, y:50}));
+            best.physics.bodies.forEach(body => Body.setVelocity(body, {x: 0, y:50}));
             break;
         }
     })
